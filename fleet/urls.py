e4c5289145ef62f6b09d38c5d9d20af84a482745
suctiontomb <fanieduminy7@gmail.com>
2026-05-07 12:36:29 +0300

    Add '/' convention to url: api/token/refresh

--- fleet/urls.py
+++ fleet/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('vehicles.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh', TokenRefreshView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
