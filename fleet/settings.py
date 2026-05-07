5f714be3f1d19d1895fca15944e4697d16a364f2
suctiontomb <fanieduminy7@gmail.com>
2026-05-07 11:00:01 +0300

    Migrate from SQLite to PostgeSQL

--- fleet/settings.py
+++ fleet/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'fleet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fleet_db',
+        'USER': 'fleet_user',
+        'PASSWORD': 'fleet123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
