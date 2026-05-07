d8e6db41537563ee0e1b8e65d3bf3c3c93258e61
suctiontomb <fanieduminy7@gmail.com>
2026-05-07 12:30:38 +0300

    Add JWT authentication to API endpoints

--- fleet/settings.py
+++ fleet/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import rest_framework_simplejwt
+from rest_framework.templatetags import rest_framework
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +126,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# DRF - JWT authentication
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_RENDERER_CLASSES':(
+        'rest_framework.renderers.JSONRenderer',
+    ),
+
+}
--- vehicles/views.py
+++ vehicles/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 from vehicles.models import Vehicle, Mechanic,MaintenanceJob
 from rest_framework import  viewsets
 from vehicles.serializers import VehicleSerializer, MechanicSerializer, MaintenanceJobSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
 class MechanicViewSet(viewsets.ModelViewSet):
     queryset = Mechanic.objects.all()
     serializer_class = MechanicSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class MaintenanceViewSet(viewsets.ModelViewSet):
     queryset = MaintenanceJob.objects.all()
     serializer_class = MaintenanceJobSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
