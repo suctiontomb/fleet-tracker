0c0e670d39ea9a82682462e7b06bf5a7ee8c2a91
suctiontomb <fanieduminy7@gmail.com>
2026-05-14 16:18:34 +0300

    Extend JWT token lifetime for development

--- fleet/settings.py
+++ fleet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,3 +150,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
+SIMPLE_JWT = {
+    'ACESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
